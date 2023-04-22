c51cbf3c9d3349ce1542c1e35d3be19935701e4d
whisenhunt-robert <dahweirdacus@gmail.com>
2023-04-21 21:31:14 -0400

    Update JS file to include as much of the footer.

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,2 +1,4 @@
+document.querySelector("#year").innerHTML = "&copy; 2023 | Robert Whisenhunt | North Carolina";
+
 let lastUpdated = new Date(document.lastModified);
 document.getElementById("lastUpdated").textContent = lastUpdated;

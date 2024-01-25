c59451dfab574bc9b34b6514844eb8f1bb906823
Robert Whisenhunt <dahweirdacus@gmail.com>
2024-01-25 04:18:16 -0500

    Been trying to get the map to be hidden when clicking hambtn.

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -5,3 +5,9 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+// Supposed to hide map when hamburger button is open
+function toggleMapVisibility() {
+    let mapContainer = document.getElementById('map-container');
+    mapContainer.style.overflow = (mapContainer.style.overflow === 'hidden') ? 'visible' : 'hidden';
+  }

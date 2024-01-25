ad4099d3262be349194e928a07c962828c52dfcd
Robert Whisenhunt <dahweirdacus@gmail.com>
2024-01-25 00:33:42 -0500

    Possibly got the update home page working.

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,31 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
-	const hamBtn = document.getElementById('menu-btn');
-	const closeBtn = document.getElementById('close-btn');
-	const nav = document.querySelector('#navigation');
-  
-	hamBtn.addEventListener('click', function() {
-		
-	   // Apply scale
-	   hamBtn.style.transform = 'scale(0.90)';
-  
-	   setTimeout(function(){
-		  hamBtn.transform = 'scale(1)';
-		  nav.classList.remove('nav-menu-close');
-		  closeBtn.classList.remove('close-close-btn');
-		  hamBtn.classList.add('close-ham-btn');
-	   }, 150);
-	});
-  
-	closeBtn.addEventListener('click', function(){
-  
-	   closeBtn.style.transform = 'scale(0.90)';
-	  
-		// Revert scale after a delay
-		setTimeout(function() {
-		  closeBtn.style.transform = 'scale(1)';
-		  nav.classList.add('nav-menu-close');
-		  hamBtn.classList.remove('close-ham-btn');
-		  closeBtn.classList.add('close-close-btn');
-	  }, 150);
-	});
- });
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});

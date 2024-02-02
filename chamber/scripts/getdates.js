61d9758063630e33ebc096a482daf6c0d2315370
Robert Whisenhunt <dahweirdacus@gmail.com>
2024-02-01 20:34:34 -0500

    Chamber Page a work in progress.

---
+++ chamber/scripts/getdates.js
@@ -0,0 +1,5 @@
+let currentYear = new Date().getFullYear();
+document.getElementById("currentYear").textContent = currentYear;
+
+let lastModifiedDate = document.lastModified;
+document.getElementById("lastModified").textContent = lastModifiedDate;

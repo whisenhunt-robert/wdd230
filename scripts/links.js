a792689240cdfecdcfa556c3115cbcb3fbf41ddb
Robert Whisenhunt <dahweirdacus@gmail.com>
2024-03-06 22:54:54 -0500

    Trying to fix my json stuff.

--- scripts/links.js
+++ scripts/links.js
@@ -1,13 +1,37 @@
 const baseURL = "https://whisenhunt-robert.github.io/wdd230/";
 const linksURL = "https://whisenhunt-robert.github.io/wdd230/data/links.json";
+const output = document.getElementById("output");
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
     displayLinks(data);
-    console.log(response, data);
 }
 
+function displayLinks(data) {
+    let allLessons = data.lessons;
+    const line = document.createElement('li');
+    const pizza = ''
+    allLessons.forEach(function(allLessons) {
+        let lessonNum = allLessons.lesson;
+        let lessonUrl = allLessons.links;
+        lessonUrl.forEach(function(lessonsUrl){
+            let url = lessonsUrl.url;
+            let title = lessonsUrl.title;
+            // Create new HTML elements for each link
+            const listItem = document.createElement('li');
+            const anchor = document.createElement('a');
+            anchor.href = url;
+            anchor.textContent = title;
 
+            // Append the anchor element to the list item
+            listItem.appendChild(anchor);
+
+            // Append the list item to the output div
+            output.appendChild(listItem);
+        })
+    })
+}
 
 getLinks();
+displayLinks();
